6ab4754301a4ad38543dc2a82226fe503bc49aa6
Gerald Gradwohl <gerald@duncrow.com>
2022-07-04 12:24:08 +0200

    languages

--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -5,3 +5,4 @@ $GLOBALS['TL_LANG']['tl_content']['dc_playcanvas_settings'][1] = 'iFrame Url fü
 $GLOBALS['TL_LANG']['tl_content']['dc_playcanvas_settings_files'][0] = 'Wählen Sie ein ZIP-File aus';
 $GLOBALS['TL_LANG']['tl_content']['dc_playcanvas_settings_files'][1] = 'Nach dem Speichern wird das Zip file in FileManager/dc-playcanvas-unzipped extrahiert und die index.htlm in das URL Feld gespeichert.';
 $GLOBALS['TL_LANG']['tl_content']['dc_playcanvas_type_legend'] = 'Playcanvas Settings';
+$GLOBALS['TL_LANG']['tl_content']['dc_playcanvas_legend'] = 'Playcanvas Settings';
--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -5,3 +5,4 @@ $GLOBALS['TL_LANG']['tl_content']['dc_playcanvas_settings'][1] = 'Iframe Url to 
 $GLOBALS['TL_LANG']['tl_content']['dc_playcanvas_settings_files'][0] = 'Select a Zip file';
 $GLOBALS['TL_LANG']['tl_content']['dc_playcanvas_settings_files'][1] = 'Click save to extract the content in FileManager/dc-playcanvas-unzipped and link it in the Url field';
 $GLOBALS['TL_LANG']['tl_content']['dc_playcanvas_type_legend'] = 'Playcanvas Settings';
+$GLOBALS['TL_LANG']['tl_content']['dc_playcanvas_legend'] = 'Playcanvas Settings';
